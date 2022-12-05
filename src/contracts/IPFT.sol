15bf8e75b203b64af97cc4e1c21c56baf13ef597
Vlad Faust <hey@vladfaust.com>
2022-12-05 17:20:21 +0400

    refactor(IPFT): improve docs, rename args

--- src/contracts/IPFT.sol
+++ src/contracts/IPFT.sol
@@ -14,17 +14,22 @@ library IPFT {
      *
      * magic    | version           | chain id        | contract address | author address
      * 4 bytes  | 4 bytes           | 8 bytes         | 20 bytes         | 20 bytes
-     * `"ipft"` | `0x0165766d` [^1] | `block.chainid` | `contractAddr`   | `authorAddr`
+     * `"ipft"` | `0x0165766d` [^1] | `block.chainid` | `contract_`      | `author`
      *
      * [^1]: ASCII string `\x{01}evm`.
+     *
+     * @param content   The file containing an IPFT tag.
+     * @param tagOffset The IPFT tag offset in bytes.
+     * @param contract_ The proving contract address.
+     * @param author    The to-become-token-author address.
      *
      * @return hash The keccak256 hash of `content`.
      */
     function verifyTag(
         bytes calldata content,
         uint32 tagOffset,
-        address contractAddr,
-        address authorAddr
+        address contract_,
+        address author
     ) external view returns (bytes32 hash) {
         // Check the content length so that it may contain the tag.
         require(content.length >= tagOffset + 56, "IPFT: content too short");
@@ -43,13 +48,13 @@ library IPFT {
 
         // Check the tag contract address.
         require(
-            _bytesToAddress(content, tagOffset + 16) == contractAddr,
+            _bytesToAddress(content, tagOffset + 16) == contract_,
             "IPFT: invalid contract address"
         );
 
         // Check the tag author.
         require(
-            _bytesToAddress(content, tagOffset + 36) == authorAddr,
+            _bytesToAddress(content, tagOffset + 36) == author,
             "IPFT: invalid author address"
         );
 
